46cbd7ec576e3ba5cc22ce599afe1869fb20ad71
FaBBar13 <fab.bar@laposte.net>
2023-06-30 16:28:20 +0200

    ...avancement...

--- src/index.php
+++ src/index.php
@@ -3,48 +3,33 @@
 
 $title = 'mon titre de malade';
 
-// $data = file_get_contents(__DIR__ . '/../data/info.txt');
-
-// file_put_contents(__DIR__ . '/../data/info.txt', 'fdlfkldfk');
-
-// var_dump($data);
-
-// definition des colonnes du fichier
 $info = "vide";
+
+
 
 $liste = [];
 
-$taille_0 = 1;
-$taille_1 = 10;
-$taille_2 = 5;
-$taille_3 = 1;
-$taille_4 = 30;
+$source = file_get_contents('../data/info.txt');
 
+// var_dump($source);
+// echo ("<br>");
+$lignes = explode("\n", $source);
+// var_dump($ligne);
+// echo ("<br>");
 
-$source = fopen('../data/info.txt', 'a+');
-
-
-while (!feof($source)) {
-    //echo var_dump($source);
-    $check = fread($source, $taille_0);
-    $date = fread($source, $taille_1);
-    $heure = fread($source, $taille_2);
-    $statut = fread($source, $taille_3);
-    $tache = fread($source, $taille_4);
-
-    $retour_chariot = fread($source, 1); //La fin d'une ligne est marquée par 1 octet de retour chariot
-
-
-
-    // 
-
-    $liste[] = ['check' => $check, 'date' => $date, 'heure' => $heure, 'statut' => $statut, 'tache' => $tache];
-    //echo "<tr><td>$date</td><td>$heure</td><td>$tache</td><td>$statut</td></tr>";
+for ($z = 0; $z < count($lignes) - 1; $z++) {
+    if (empty($lignes[$z])) {
+        continue;
+    }
+    $detail = explode("%", $lignes[$z]);
+    $liste[] = [
+        'check' => $detail[0],
+        'date' => $detail[1],
+        'heure' => $detail[2],
+        'statut' => $detail[3],
+        'tache' => $detail[4]
+    ];
 }
-;
-fclose($source);
-
-
 
 
 
--- src/vues/home.php
+++ src/vues/home.php
@@ -1,4 +1,9 @@
-<?php include __DIR__ . '/header.php'; ?>
+<?php
+// Aymeric : stocke le html ds un buffer et à la fin du code , mettre ob_flush() pour la chasse d'eau du buffer vers la page
+// permet d'utilise header(location)
+ob_start();
+
+include __DIR__ . '/header.php'; ?>
 <div class="container">
     <h1>Tâches à Faire</h1>
 
@@ -7,7 +12,7 @@
 
         <thead>
             <tr>
-                <th> Sel. </th>
+                <th> Action </th>
                 <th> Date </th>
                 <th> Heure </th>
                 <th> Statut </th>
@@ -19,34 +24,80 @@
         echo "<h3>$info</h3>";
         echo "<br>";
         echo ('nb lignes= ' . count($liste));
+        echo "<br>";
 
-        foreach ($liste as $entry):
-            echo "<tr><td><input type='checkbox' id='{$entry['check']}'><td><td>{$entry['date']}</td><td>{$entry['heure']}</td><td>{$entry['statut']}</td><td>{$entry['tache']}</td></tr>";
-        endforeach; ?>
+        $input_data = [];
+        function majFichier()
+        {
+            global $liste;
+            for ($i = 0; $i < count($liste); $i++) {
+                $lg = $liste[$i];
+                $newcontent .= $lg['check'] . '%' . $lg['date'] . '%' . $lg['heure'] . '%' . $lg['statut'] . '%' . $lg['tache'] . "\n";
+            }
+
+            file_put_contents('../data/info.txt', $newcontent);
+            header("Location: ./");
+            exit;
+        }
+        //var_dump($liste);
+        foreach ($liste as $line => $entry):
+            //var_dump($entry);
+            //echo "<tr><td><input type='checkbox' " . (!!$entry['check'] ? 'checked' : '') . "></td><td>{$entry['date']}</td><td>{$entry['heure']}</td><td>{$entry['statut']}</td><td>{$entry['tache']}</td></tr>";
+        
+
+            // ajouter test sur ligne 0 avec isset et non empty
+            // + voir PHP_EOL et ranger le tableau modifié avec $contenu = array_values($contenu);
+        
+
+            $chaine_btn = "<form method='post' class='boboy'>
+            <input type='hidden' name='line' value='{$line}'>
+            <button name='action' type='submit' value='mod' class='btn btn-warning bouton'>Modif.</button>
+            <button name='action' type='submit' value='sup' class='btn btn-danger bouton'>Suppr</button>
+                </form>";
+
+            echo "<tr><td>" . $chaine_btn . "</td><td>{$entry['date']}</td><td>{$entry['heure']}</td><td>{$entry['statut']}</td><td>{$entry['tache']}</td></tr>";
+        endforeach;
+        ?>
 
     </table>
 
-    <div class="d-flex justify-content-around">
-        <!-- <button type="button" name="action" value="add" class="btn btn-primary bouton">Ajouter</button> -->
-        <button type="button" name="action" value="mod" class="btn btn-primary bouton">Modifier</button>
-        <button type="button" name="action" value="sup" class="btn btn-primary bouton">Supprimer</button>
-    </div>
+
     <?php
 
-    // switch ($_POST['action']) {
-    //     // case "add":
-    //     //     echo "ajout tache";
-    //     //     break;
-    //     case "mod":
-    //         echo "modif. tache ";
-    //         break;
-    //     case "sup":
-    //         echo "suppr. tache";
-    //     default:
-    //         echo "bizarre...";
-    
-    // }
-    // ;
+
+
+    function Suppr($par_numlig)
+    {
+        echo "suppr. tache N°= " . $par_numlig;
+        echo ('<br>');
+        global $liste;
+        unset($liste[$par_numlig]);
+        $liste = array_values($liste);
+        majFichier();
+        //var_dump($liste);
+    }
+
+
+    if (!empty($_POST['action'])) {
+
+        //var_dump($_POST['action']);
+        switch ($_POST['action']) {
+            case "edit":
+                echo "ajout tache";
+                break;
+            case "mod":
+                echo "modif. tache ";
+                break;
+            case "sup":
+                //echo ($_POST['line']);
+                Suppr($_POST['line']);
+                break;
+            default:
+                echo "bizarre...";
+        }
+    }
+
+
     ?>
 
     <div class="ajout">
@@ -54,13 +105,21 @@
         <form method="post">
 
             <div class="form-group">
-                <input type="date" placeholder="Date" id="date" name="date" maxlength="8"
+                <input type="date" placeholder="Date" id="date" name="date" maxlength="8" class="form-control"
+                    value="<?= $input_data['date'] ?? '' ?>"></input><br />
+                <!-- <label for="time"> et l'horloge ???</label> -->
+                <input type="time" id="heure" name="time" value="<?= $input_data['heure'] ?? '' ?>"
                     class="form-control"></input><br />
-                <label for="time"></label>
-                <input type="time" id="heure" name="time" class="form-control"></input><br />
-                <input type="text" placeholder="Action..." id="action" name="action" maxlength="30"
-                    class="form-control"></input><br />
-                <button class="btn btn-primary" type="submit">Valider</button>
+                <input type="text" placeholder="Action..." id="action" name="action" maxlength="20"
+                    value="<?= $input_data['tache'] ?? '' ?>" class="form-control"></input><br />
+                <?php if (!empty($input_data)) { ?>
+                    <input type="hidden" name="line" value="<?= $input_data['line'] ?>">
+                    <button class="btn btn-primary" type="submit" name="action" value="edit">Modifier</button>
+
+                <?php } else { ?>
+                    <button class="btn btn-success" type="submit" name="action" value="add">Valider</button>
+                <?php } ?>
+
             </div>
 
 
@@ -69,43 +128,36 @@
     <?php
 
     // echo ($_POST['date'] . ' ' . $_POST['heure'] . ' ' . $_POST['action']);
-    
-    if (!empty($_POST['date']) && !empty($_POST['heure']) && !empty($_POST['action'])) {
+    if (!empty($_POST['date']) && !empty($_POST['time']) && !empty($_POST['action'])) {
+
         $date_sai = $_POST['date'];
-        $heure_sai = $_POST['heure'];
+        $heure_sai = $_POST['time'];
         $action_sai = $_POST['action'];
-
+        // echo ('VALIDATION');
+        // echo "<br>";
         $newcontent = "";
-
-        // echo ($date_sai . ' ' . $heure_sai . ' ' . $action_sai);
-        $liste[] = ['check' => 0, 'date' => $date_sai, 'heure' => $heure_sai, 'statut' => 1, 'tache' => $action_sai];
-        // var_dump($liste);
-        //echo "<br>";
-        //echo ('nb lignes= ' . count($liste));
-    
+        $liste[] = ['check' => 0, 'date' => $date_sai, 'heure' => $heure_sai, 'statut' => '1', 'tache' => $action_sai];
         //var_dump($liste);
-        //$destination = fopen('../data/info.txt', 'w');
-        for ($i = 0; $i < count($liste); $i++) {
-            $detail = $liste[$i];
+        echo "<br>";
 
-
-            //fwrite($destination, $detail['check'] . $detail['date'] . $detail['heure'] . $detail['statut'] . $detail['tache'] . "\n");
-            $newcontent .= $detail['check'] . $detail['date'] . $detail['heure'] . $detail['statut'] . $detail['tache'] . "\n";
-            //print_r($detail['check'] . $detail['date'] . $detail['heure'] . $detail['tache'] . $detail['statut'] . '<br>');
-        }
-        ;
-        var_dump($newcontent);
-        //fclose($destination);
-        // echo ($newcontent);
-        file_put_contents('../data/info.txt', $newcontent);
-
-        //  print_r($liste[0]);
+        majFichier();
+        // for ($i = 0; $i < count($liste); $i++) {
+        //     $lg = $liste[$i];
+        //     $newcontent .= $lg['check'] . '%' . $lg['date'] . '%' . $lg['heure'] . '%' . $lg['statut'] . '%' . $lg['tache'] . "\n";
+        // }
     
+        // file_put_contents('../data/info.txt', $newcontent);
+        // header("Location: ./");
+        // exit;
     }
-    ;
+
 
     ?>
 
 </div>
 
-<?php include __DIR__ . '/footer.php'; ?>
+<?php
+
+ob_flush();
+
+include __DIR__ . '/footer.php'; ?>
